17b6d74e99bdcacf043dc9f03d354dd6cbd62830
bloodfunny <adhyaakmalrafiansyah@email.com>
2024-05-03 23:11:12 +0700

    langkah membuat node baru

--- 0139_Doublelinkedlist/0139_Doublelinkedlist.cpp
+++ 0139_Doublelinkedlist/0139_Doublelinkedlist.cpp
@@ -26,7 +26,13 @@ void addNode()
 			cout << "\033[31mDuplicate roll numbers not allowed\033[0m" << endl;
 			return;
 		}
-		
+		newNode->next = START;
+		if (START != NULL)
+		{
+			START->prev = newNode;
+		}
+		newNode->prev = NULL;
+		START = newNode;
 	}
 
 }
