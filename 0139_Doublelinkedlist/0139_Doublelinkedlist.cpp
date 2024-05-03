115423641a7f9cc06fd852967e5fcd3f9ebcbe45
bloodfunny <adhyaakmalrafiansyah@email.com>
2024-05-04 00:12:51 +0700

    kondisi jika node harus dihapus di node pertama

--- 0139_Doublelinkedlist/0139_Doublelinkedlist.cpp
+++ 0139_Doublelinkedlist/0139_Doublelinkedlist.cpp
@@ -93,6 +93,18 @@ void deleteNode()
 
 	while (current != NULL && current->noMhs != ro11No)
 	{
+		previous = current;
+		current = current->next;
+	}
+
+	if (current == NULL)
+	{
+		cout << "\033{31mThe record with roll number" << ro11No << "not found\033{0m" << endl;
+		return;
+	}
+
+	if (current == START)
+	{
 
 	}
 }
