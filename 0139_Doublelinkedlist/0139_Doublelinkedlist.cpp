2c14d9ff9e5bfd0b68b595261c0b9b34a189f9ab
bloodfunny <adhyaakmalrafiansyah@email.com>
2024-05-04 05:16:20 +0700

    membuat fungsi void search data

--- 0139_Doublelinkedlist/0139_Doublelinkedlist.cpp
+++ 0139_Doublelinkedlist/0139_Doublelinkedlist.cpp
@@ -152,6 +152,19 @@ void revtraverse()
 		cout << "\nList is empty" << endl;
 	else
 	{
-
+		cout << "\nRecords in ascending order of ro11 number are: " << endl;
+		Node* currentNode = START;
+		while (currentNode->next != NULL)
+			currentNode - currentNode->next;
+		while (currentNode != NULL)
+		{
+			cout << currentNode->noMhs << "" << currentNode->name << endl;
+			currentNode = currentNode->prev;
+		}
 	}
+}
+
+void searchData()
+{
+
 }
