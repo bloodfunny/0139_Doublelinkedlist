0369ae07e97fa7a78357dbf49f5ef2d67dcd322a
bloodfunny <adhyaakmalrafiansyah@email.com>
2024-05-04 13:52:57 +0700

    tukar case 1-6

--- 0139_Doublelinkedlist/0139_Doublelinkedlist.cpp
+++ 0139_Doublelinkedlist/0139_Doublelinkedlist.cpp
@@ -202,7 +202,27 @@ int main()
 			char ch;
 			cin >> ch;
 
-
+			switch (ch)
+			{
+			case '1':
+				addNode();
+				break;
+			case '2':
+				deleteNode();
+				break;
+			case '3':
+				traverse();
+				break;
+			case '5':
+				revtraverse();
+				break;
+			case '6':
+				return 0;
+			default:
+				cout << "\nInvalid option" << endl;
+				break;
+			}
 		}
+
 	}
 }
